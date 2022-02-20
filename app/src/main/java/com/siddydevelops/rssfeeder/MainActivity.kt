5ec3639788e25fb38928a1dbfece23aa1e606a7b
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-02-20 08:59:31 +0530

    Added AsyncTask

--- app/src/main/java/com/siddydevelops/rssfeeder/MainActivity.kt
+++ app/src/main/java/com/siddydevelops/rssfeeder/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.siddydevelops.rssfeeder
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"OnCreate called")
+
     }
+
+    private inner class DownloadData: AsyncTask<String, Void, String>()
+    {
+        private val TAG = "DownloadData"
+        override fun doInBackground(vararg p0: String?): String {
+            TODO("Not yet implemented")
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+    }
+
 }
